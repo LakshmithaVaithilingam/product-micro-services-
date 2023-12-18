3909d486b9cb1e9ac8e6e997a9bc6d927ece1d4d
Lakshmitha Vaithilingam <lakshmitha.20210713@iit.ac.lk>
2023-12-18 18:54:31 +0530

    created migration file
    Migration file for age group and discount

---
+++ product/database/migrations/2023_12_18_094728_add_age_group_and_discount_to_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('age_group')->nullable();
+            $table->decimal('discount', 5, 2)->default(0.00);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('age_group');
+            $table->dropColumn('discount');
+        });
+    }
+};
